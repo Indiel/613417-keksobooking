633cad592d4c0df5a410643d006c5e08c975cd29
Indiel <kori.yanagi@gmail.com>
2018-06-24 15:47:57 +0300

    Исправлено оформление

--- js/map.js
+++ js/map.js
@@ -1,24 +1,28 @@
 'use strict';
 
-var TITLES = ['Большая уютная квартира',
-              'Маленькая неуютная квартира',
-              'Огромный прекрасный дворец',
-              'Маленький ужасный дворец',
-              'Красивый гостевой домик',
-              'Некрасивый негостеприимный домик',
-              'Уютное бунгало далеко от моря',
-              'Неуютное бунгало по колено в воде'];
+var TITLES = [
+  'Большая уютная квартира',
+  'Маленькая неуютная квартира',
+  'Огромный прекрасный дворец',
+  'Маленький ужасный дворец',
+  'Красивый гостевой домик',
+  'Некрасивый негостеприимный домик',
+  'Уютное бунгало далеко от моря',
+  'Неуютное бунгало по колено в воде'
+];
 var TYPES = {
-            palace: 'Дворец',
-            flat: 'Квартира',
-            house: 'Дом',
-            bungalo: 'Бунгало'
-            };
+  palace: 'Дворец',
+  flat: 'Квартира',
+  house: 'Дом',
+  bungalo: 'Бунгало'
+};
 var TIMES = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-              'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-              'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
 
 document.querySelector('.map').classList.remove('map--faded');
 
